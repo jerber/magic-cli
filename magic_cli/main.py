d5ddc244e84a1575bc5136e9257ba70f563c68ec
Jeremy Berman <jerber@sas.upenn.edu>
2020-06-23 00:53:12 -0400

    adding more

--- magic_cli/main.py
+++ magic_cli/main.py
@@ -19,12 +19,30 @@ def sowell():
 
 
 def download_boilerplate_git_folder(output_filename):
-    github_repo_url = "https://api.github.com/repos/jerber/magic-cli/tarball/master"
+    github_repo_url = "https://api.github.com/repos/jerber/magic-server/tarball/master"
     r = requests.get(github_repo_url, stream=True)
     with open(output_filename, "wb") as f:
         for chunk in r.raw.stream(1024, decode_content=False):
             if chunk:
                 f.write(chunk)
+
+
+def run_from_scratch(project_path: str):
+
+    # first chdir into it
+    os.chdir(project_path)
+
+    # then create venv and activate it
+    os.system("which python")
+
+    os.system("python3 -m venv venv")
+    os.system(
+        "source venv/bin/activate && which python && pip install -r requirements.txt && python main.py"
+    )
+
+    # os.system("which python")
+
+    # then pip install requirements
 
 
 @app.command()
@@ -35,6 +53,7 @@ def create(project_name: str = typer.Argument("magic-server"), replace: bool = F
             "There is already a project that exists. If you would like to replace this project, "
             "add the flag --replace to your command"
         )
+        raise typer.Exit()
 
     zip_filename = "boilerplate.tar.gz"
     download_boilerplate_git_folder(zip_filename)
